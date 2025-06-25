116a6af10581503612be6a09ec204a0d641c4f35
Swapnil Roy <101203567+linpaws1364@users.noreply.github.com>
2025-06-25 21:06:23 +0530

    Update ProductManagement.java

--- src/main/java/com/Osom/demo/controller/ProductManagement.java
+++ src/main/java/com/Osom/demo/controller/ProductManagement.java
@@ -15,7 +15,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "https://osom-in-clone-frontend.vercel.app")
 @RequestMapping("product")
 @RestController
 public class ProductManagement {
