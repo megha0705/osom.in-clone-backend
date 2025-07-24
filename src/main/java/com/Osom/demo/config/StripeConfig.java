942b5e41f9f5dab820b6796f72bd7195ee0cb5e5
Reshmi Chakraborty <149001562+megha0705@users.noreply.github.com>
2025-07-24 19:46:36 +0530

    Update StripeConfig.java

--- src/main/java/com/Osom/demo/config/StripeConfig.java
+++ src/main/java/com/Osom/demo/config/StripeConfig.java
@@ -8,11 +8,15 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class StripeConfig {
-    public static final String SECRET_KEY = "";
+  public StripeConfig() {
+        String secretKey = System.getenv("STRIPE_SECRET_KEY");
 
-    public StripeConfig(){
-       Stripe.apiKey = SECRET_KEY;
-   }
+        if (secretKey == null || secretKey.isEmpty()) {
+            throw new RuntimeException("STRIPE_SECRET_KEY is not set in the environment.");
+        }
+
+        Stripe.apiKey = secretKey;
+    }
 
    public PaymentIntent createPaymentIntent(double amount , String currency)throws StripeException {
        PaymentIntentCreateParams params = PaymentIntentCreateParams.builder().setAmount((long)amount).setCurrency(currency).build();
